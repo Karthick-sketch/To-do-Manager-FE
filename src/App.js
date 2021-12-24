f7599ea9229b205918703a1716067c30ea0b1e32
“Karthick-Ramesh-16” <karthick.ramesh@freshworks.com>
2021-12-25 00:35:02 +0530

    Move functions to App file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function TodoCategory(props) {
+  const [todos, setTodos] = useState(props.data);
+
+  function completedTodosCount() {
+    let count = 0;
+    for (let i of props.data) {
+      if (i.completed) count++;
+    }
+
+    return count;
+  }
+
+  return <section className="TodoSection">
+    <div className="TodoSectionTitle">
+      <h4 className="TodoSectionTitle-text">{props.name}</h4>
+      <p className="TodoSectionTitle-remainingCount">{completedTodosCount()} / {todos.length}</p>
     </div>
+    <TodoItems data={todos}/>
+  </section>;
+}
+
+function TodoItems(props) {
+  const arr = props.data;
+
+  function checked(todo) {
+    if (todo.completed) {
+      return <input type="checkbox" name="completed" className="TodoItem-checkbox" checked/>;
+    } else {
+      return <input type="checkbox" name="completed" className="TodoItem-checkbox"/>;
+    }
+  }
+
+  function strickoutText(todo) {
+    if (todo.completed) {
+      return <p className="TodoItem-text">{todo.todo_text}</p>
+    } else {
+      return <p>{todo.todo_text}</p>
+    }
+  }
+
+  const listItems = arr.map(todo =>
+    <li key={todo.id}>
+      <label className="TodoItem-container">
+        <div>
+          <form action={"#/" + String(todo.id)}>{checked(todo)}</form>
+          {strickoutText(todo)}
+          <p className="TodoItem-dueDate">{todo.due_date}</p>
+        </div>
+        <form action="#">
+          <button className="TodoItem-delete">
+            <img src="trashcan.png" alt="Delete todo"/>
+          </button>
+        </form>
+      </label>
+    </li>
   );
+
+  return <ul className="TodoItem">{listItems}</ul>;
+}
+
+function App(props) {
+  const date = new Date();
+  const today = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
+  const [overdue, setOverdue] = useState(props.data.filter(todo => todo.due_date < today));
+  const [due_today, setDueToday] = useState(props.data.filter(todo => todo.due_date === today));
+  const [due_later, setDueLater] = useState(props.data.filter(todo => todo.due_date > today));
+
+  return <div>
+    <TodoCategory data={overdue} name="Overdue" value={true}/>
+    <TodoCategory data={due_today} name="Due Today" value={false}/>
+    <TodoCategory data={due_later} name="Due Later" value={true}/>
+  </div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,92 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
 function AddTodoForm() {
-  return <form action="#">
-    <div>
-      <input name="todo-text" className="AddTodo-text" placeholder="What's next?" autoComplete="off" /> {/* autoFocus */}
-      <input type="date" name="due_date" className="AddTodo-date"/>
-      <button type="submit" className="AddTodo-button">Add</button>
-    </div>
+  return <form action="#" className="AddTodo">
+    <input name="todo-text" className="AddTodo-text" placeholder="What's next?" autoComplete="off" /> {/* autoFocus */}
+    <input type="date" name="due_date" className="AddTodo-date"/>
+    <button type="submit" className="AddTodo-button">Add</button>
   </form>;
 }
 
-function TodoItems(props) {
-  const arr = props.data;
-
-  function checked(todo) {
-    if (todo.completed) {
-      return <input type="checkbox" name="completed" className="TodoItem-checkbox" checked/>;
-    } else {
-      return <input type="checkbox" name="completed" className="TodoItem-checkbox"/>;
-    }
-  }
-
-  function strickoutText(todo) {
-    if (todo.completed) {
-      return <p className="TodoItem-text">{todo.todo_text}</p>
-    } else {
-      return <p>{todo.todo_text}</p>
-    }
-  }
-
-  const listItems = arr.map(todo =>
-    <li key={todo.id}>
-      <label className="TodoItem-container">
-        <div>
-          <form action={"#/" + String(todo.id)}>{checked(todo)}</form>
-          {strickoutText(todo)}
-          <p className="TodoItem-dueDate">{todo.due_date}</p>
-        </div>
-        <form action="#">
-          <button className="TodoItem-delete">
-            <img src="trashcan.png" alt="Delete todo"/>
-          </button>
-        </form>
-      </label>
-    </li>
-  );
-
-  return <ul className="TodoItem">{listItems}</ul>;
-}
-
-function TodoCategory(props) {
-  const [todos, setTodos] = useState(props.data);
-
-  function completedTodosCount() {
-    let count = 0;
-    for (let i of props.data) {
-      if (i.completed) count++;
-    }
-
-    return count;
-  }
-
-  return <section className="TodoSection">
-    <div className="TodoSectionTitle">
-      <h4 className="TodoSectionTitle-text">{props.name}</h4>
-      <p className="TodoSectionTitle-remainingCount">{completedTodosCount()} / {todos.length}</p>
-    </div>
-    <TodoItems data={todos}/>
-  </section>;
-}
-
-function TodoSections(props) {
-  const date = new Date();
-  const today = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
-  const [overdue, setOverdue] = useState(props.data.filter(todo => todo.due_date < today));
-  const [due_today, setDueToday] = useState(props.data.filter(todo => todo.due_date === today));
-  const [due_later, setDueLater] = useState(props.data.filter(todo => todo.due_date > today));
-
-  return <div>
-    <TodoCategory data={overdue} name="Overdue" value={true}/>
-    <TodoCategory data={due_today} name="Due Today" value={false}/>
-    <TodoCategory data={due_later} name="Due Later" value={true}/>
-  </div>;
-}
 
 const Todos = [
   {id: 1, todo_text: "Buy movie ticket", due_date: "2021-12-24", completed: false},
@@ -97,7 +23,7 @@ const Todos = [
 ReactDOM.render(
   <article className="Todo-container">
 	  <h1>My To-do List</h1>
-    <div className="TodoPage-content"><AddTodoForm/> <br/> <TodoSections data={Todos}/></div>
+    <div className="TodoPage-content"><AddTodoForm/> <br/> <App data={Todos}/></div>
   </article>,
   document.getElementById('root')
 );
