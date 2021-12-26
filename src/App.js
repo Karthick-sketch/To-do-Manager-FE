3517b2ad1f843e59c2374f9bd89d7ff6d4d54094
“Karthick-Ramesh-16” <karthick.ramesh@freshworks.com>
2021-12-26 23:23:17 +0530

    Add axios REST operations to Todo Manager

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import React from 'react';
+import axios from 'axios';
 
+const api = axios.create({
+  baseURL: "http://localhost:8080/"
+})
 
 function TodoItems(props) {
-  const [decoration, setDecoration] = useState("none");
+  function isNotToday(due_date) {
+    return (props.value ? <p className="TodoItem-dueDate">{due_date}</p> : null);
+  }
+
   const arr = props.data;
-
-  function handleChange(e) {
-    setDecoration((e.target.checked ? "line-through" : "none"));
-  }
-
-  function isNotToday(todo) {
-    return (props.value ? <p className="TodoItem-dueDate">{todo.due_date}</p> : null);
-  }
-
   const listItems = arr.map(todo =>
     <li key={todo.id}>
       <label className="TodoItem-container">
         <div>
-          <form action={"#/" + String(todo.id)}>
-            <input type="checkbox" name="completed" className="TodoItem-checkbox" onClick={handleChange}/>
+          <form action={String(todo.id)}>
+            <input type="checkbox" name="completed" className="TodoItem-checkbox"/>
           </form>
-          <p style={{textDecoration: decoration}}>{todo.todo_text}</p>
-          {isNotToday(todo)}
+          <p>{todo.todo_text}</p>
+          {isNotToday(todo.due_date)}
         </div>
         <form action="#">
           <button className="TodoItem-delete">
@@ -38,8 +35,6 @@ function TodoItems(props) {
 }
 
 function TodoCategory(props) {
-  const [todos, setTodos] = useState(props.data);
-
   function completedTodosCount() {
     let count = 0;
     for (let i of props.data) {
@@ -52,24 +47,74 @@ function TodoCategory(props) {
   return <section className="TodoSection">
     <div className="TodoSectionTitle">
       <h4 className="TodoSectionTitle-text">{props.name}</h4>
-      <p className="TodoSectionTitle-remainingCount">{completedTodosCount()} / {todos.length}</p>
+      <p className="TodoSectionTitle-remainingCount">{completedTodosCount()} / {props.data.length}</p>
     </div>
-    <TodoItems data={todos} value={props.value}/>
+    <TodoItems data={props.data} value={props.value}/>
   </section>;
 }
 
-function App(props) {
-  const date = new Date();
-  const today = date.getFullYear() + "-" + (date.getMonth()+1) + "-" + date.getDate();
-  const [overdue, setOverdue] = useState(props.data.filter(todo => todo.due_date < today));
-  const [due_today, setDueToday] = useState(props.data.filter(todo => todo.due_date === today));
-  const [due_later, setDueLater] = useState(props.data.filter(todo => todo.due_date > today));
+function getAllTodos() {
+  return api.get("todos").then(({data}) => data );
+}
 
-  return <div>
-    <TodoCategory data={overdue} name="Overdue" value={true}/>
-    <TodoCategory data={due_today} name="Due Today" value={false}/>
-    <TodoCategory data={due_later} name="Due Later" value={true}/>
-  </div>;
+class AddTodoForm extends React.Component {
+  state = { todo_text: "", due_date: "" }
+
+  handleSubmit = async () => {
+    if (this.state.todo_text !== "" && this.state.due_date !== "") {
+      let res = await api.post("todo", this.state);
+      console.log(res);
+    }
+  }
+
+  handleTodoText = (e) => {
+    this.setState({ todo_text: e.target.value });
+    e.preventDefault();
+  }
+
+  handleDueDate = (e) => {
+    this.setState({ due_date: e.target.value });
+    e.preventDefault();
+  }
+
+  render() {
+    return <div className="AddTodo">
+      <input type="text" className="AddTodo-text" placeholder="What's next?" onChange={this.handleTodoText} autoComplete="off"/> {/*autoFocus*/}
+      <input type="date" className="AddTodo-date" onChange={this.handleDueDate}/>
+      <button className="AddTodo-button" onClick={this.handleSubmit}>Add</button>
+    </div>;
+  }
+}
+
+class App extends React.Component {
+  date = new Date();
+  today = this.date.getFullYear() + "-" + (this.date.getMonth()+1) + "-" + this.date.getDate();
+  state = { overdue: [], due_today: [], due_later: [] }
+
+  constructor() {
+    super();
+    this.getTodos();
+  }
+
+  getTodos = async () => {
+    let data = await getAllTodos();
+    this.setState({
+      overdue: data.filter(todo => todo.due_date < this.today),
+      due_today: data.filter(todo => todo.due_date === this.today), 
+      due_later: data.filter(todo => todo.due_date > this.today)
+    })
+  }
+
+  render() {
+    return <div className="TodoPage-content">
+      <AddTodoForm/> <br/>
+      <div>
+        <TodoCategory data={this.state.overdue} name="Overdue" value={true}/>
+        <TodoCategory data={this.state.due_today} name="Due Today" value={false}/>
+        <TodoCategory data={this.state.due_later} name="Due Later" value={true}/>
+      </div>
+    </div>;
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,26 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
-function AddTodoForm() {
-  return <form action="#" className="AddTodo">
-    <input name="todo-text" className="AddTodo-text" placeholder="What's next?" autoComplete="off" /> {/* autoFocus */}
-    <input type="date" name="due_date" className="AddTodo-date"/>
-    <button type="submit" className="AddTodo-button">Add</button>
-  </form>;
-}
-
-
-const Todos = [
-  {id: 1, todo_text: "Buy movie ticket", due_date: "2021-12-24", completed: false},
-  {id: 2, todo_text: "Wash clothes", due_date: "2021-12-25", completed: false},
-  {id: 3, todo_text: "Watch No Way Home", due_date: "2021-12-26", completed: false}
-];
-
 ReactDOM.render(
   <article className="Todo-container">
 	  <h1>My To-do List</h1>
-    <div className="TodoPage-content"><AddTodoForm/> <br/> <App data={Todos}/></div>
+    <App/>
   </article>,
   document.getElementById('root')
 );
